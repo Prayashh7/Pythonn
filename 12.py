adb713f1fef0ea4facb73971e9415a72737059dc
Prayash Bartaula <prayashb824@gmail.com>
2024-08-01 09:29:48 +0545

    12.py added& Readme change

---
+++ 12.py
@@ -0,0 +1,12 @@
+
+def check_odd_even():
+    try:
+        number = int(input("Enter a number: "))
+        if number % 2 == 0:
+            print(f"The number {number} is Even.")
+        else:
+            print(f"The number {number} is Odd.")
+    except ValueError:
+        print("Invalid input. Please enter a valid integer.")
+
+check_odd_even()
